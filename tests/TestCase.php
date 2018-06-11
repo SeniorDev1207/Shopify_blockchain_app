47a7153c6057e2ce35a3b081ba6f872c3fd116ec
SeniorDev1207 <rabbitdev1207@gmail.com>
2018-06-11 00:19:25 -0230

    Added model helpers for charges, fixed auth to check fo trashed stores, and to restore trashed stores, adjusted billable middleware and shop middleware

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,9 +3,11 @@
 namespace OhMyBrew\ShopifyApp\Test;
 
 use OhMyBrew\ShopifyApp\Models\Shop;
+use OhMyBrew\ShopifyApp\Models\Charge;
 use OhMyBrew\ShopifyApp\ShopifyAppProvider;
 use Orchestra\Database\ConsoleServiceProvider;
 use Orchestra\Testbench\TestCase as OrchestraTestCase;
+use Carbon\Carbon;
 
 abstract class TestCase extends OrchestraTestCase
 {
@@ -60,11 +62,16 @@ abstract class TestCase extends OrchestraTestCase
 
     protected function seedDatabase()
     {
+        $this->createShops();
+        $this->createCharges();
+    }
+
+    protected function createShops()
+    {
         // Paid shop, not grandfathered
         $shop = new Shop();
         $shop->shopify_domain = 'example.myshopify.com';
         $shop->shopify_token = '1234';
-        $shop->charge_id = 678298290;
         $shop->save();
 
         // Non-paid shop, grandfathered
@@ -84,5 +91,61 @@ abstract class TestCase extends OrchestraTestCase
         $shop = new Shop();
         $shop->shopify_domain = 'no-token-shop.myshopify.com';
         $shop->save();
+
+        // Trashed shop
+        $shop = new Shop();
+        $shop->shopify_domain = 'trashed-shop.myshopify.com';
+        $shop->save();
+        $shop->delete();
+    }
+
+    public function createCharges()
+    {;
+        $charge = new Charge();
+        $charge->charge_id = 98298298;
+        $charge->test = true;
+        $charge->name = 'Test Plan';
+        $charge->status = 'accepted';
+        $charge->type = 1;
+        $charge->price = 15.00;
+        $charge->trial_days = 7;
+        $charge->trial_ends_on = Carbon::createFromDate(2018, 6, 3, 'UTC')->addWeeks(1);
+        $charge->shop_id = Shop::where('shopify_domain', 'example.myshopify.com')->first()->id;
+        $charge->save();
+
+        $charge = new Charge();
+        $charge->charge_id = 67298298;
+        $charge->test = false;
+        $charge->name = 'Base Plan';
+        $charge->status = 'accepted';
+        $charge->type = 1;
+        $charge->price = 25.00;
+        $charge->trial_days = 7;
+        $charge->trial_ends_on = Carbon::now()->addDays(2);
+        $charge->shop_id = Shop::where('shopify_domain', 'example.myshopify.com')->first()->id;
+        $charge->save();
+
+        $charge = new Charge();
+        $charge->charge_id = 78378873;
+        $charge->test = false;
+        $charge->name = 'Base Plan Old';
+        $charge->status = 'accepted';
+        $charge->type = 1;
+        $charge->price = 25.00;
+        $charge->trial_days = 7;
+        $charge->trial_ends_on = Carbon::now()->subWeeks(4);
+        $charge->shop_id = Shop::where('shopify_domain', 'example.myshopify.com')->first()->id;
+        $charge->save();
+
+        $charge = new Charge();
+        $charge->charge_id = 89389389;
+        $charge->test = false;
+        $charge->name = 'Base Plan Old Non-Trial';
+        $charge->status = 'accepted';
+        $charge->type = 1;
+        $charge->price = 25.00;
+        $charge->trial_days = 0;
+        $charge->shop_id = Shop::where('shopify_domain', 'example.myshopify.com')->first()->id;
+        $charge->save();
     }
 }
