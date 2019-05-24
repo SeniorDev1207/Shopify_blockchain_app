1939e48ceea10ca554ed162ae07ac403eaccbf19
SeniorDev1207 <rabbitdev1207@gmail.com>
2019-05-23 21:33:03 -0230

    Added confirming test for #241

--- src/ShopifyApp/Middleware/AuthShop.php
+++ src/ShopifyApp/Middleware/AuthShop.php
@@ -21,15 +21,14 @@ class AuthShop
      * Handle an incoming request.
      *
      * @param \Illuminate\Http\Request $request
-     * @param \Closure $next
+     * @param \Closure                 $next
      *
      * @return mixed
      */
     public function handle(Request $request, Closure $next)
     {
         $validation = $this->validateShop($request);
-        if ($validation !== true)
-        {
+        if ($validation !== true) {
             return $validation;
         }
 
@@ -45,11 +44,7 @@ class AuthShop
      */
     protected function validateShop(Request $request)
     {
-        if ($request->filled('shop'))
-        {
-            session()->put('shop', $request->input('shop'));
-        }
-        $shopParam = ShopifyApp::sanitizeShopDomain(session('shop'));
+        $shopParam = ShopifyApp::sanitizeShopDomain($request->get('shop'));
         $shop = ShopifyApp::shop($shopParam);
         $session = new ShopSession($shop);
 
@@ -59,8 +54,7 @@ class AuthShop
             $shop->trashed() ||
             empty($session->getToken(true)) ||
             ($shopParam && $shopParam !== $shop->shopify_domain) === true
-        )
-        {
+        ) {
             // Either no shop session or shops do not match
             $session->forget();
 
@@ -77,7 +71,7 @@ class AuthShop
      * Come back with a response.
      *
      * @param \Illuminate\Http\Request $request
-     * @param \Closure $next
+     * @param \Closure                 $next
      *
      * @return mixed
      */
@@ -88,10 +82,8 @@ class AuthShop
         if (
             Config::get('shopify-app.esdk_enabled') &&
             ($request->ajax() || $request->expectsJson() || $request->isJson()) === false
-        )
-        {
-            if (($response instanceof BaseResponse) === false)
-            {
+        ) {
+            if (($response instanceof BaseResponse) === false) {
                 // Not an instance of a Symfony response, override
                 $response = new Response($response);
             }
