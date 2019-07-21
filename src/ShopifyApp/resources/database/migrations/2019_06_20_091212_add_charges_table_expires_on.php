fc3ba4f1345b785c41369b850808b87abd3382d9
SeniorDev1207 <rabbitdev1207@gmail.com>
2019-07-21 22:53:04 +0000

    Apply fixes from StyleCI

--- src/ShopifyApp/resources/database/migrations/2019_06_20_091212_add_charges_table_expires_on.php
+++ src/ShopifyApp/resources/database/migrations/2019_06_20_091212_add_charges_table_expires_on.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class AddChargesTableExpiresOn extends Migration
 {
@@ -26,7 +26,7 @@ class AddChargesTableExpiresOn extends Migration
     public function down()
     {
         Schema::table('charges', function (Blueprint $table) {
-            $table->dropColumn(['expires_on']);
+            //
         });
     }
 }
