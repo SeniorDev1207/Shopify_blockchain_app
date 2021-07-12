5375ca657358667f4d3779ad1a3b1b236c6d4933
SeniorDev1207 <rabbitdev1207@gmail.com>
2021-07-12 17:28:10 +0200

    Enable PHP-CS-Fixer in Github Action (#883)

--- src/ShopifyApp/Util.php
+++ src/ShopifyApp/Util.php
@@ -59,42 +59,42 @@ class Util
      * Array parameters such as `name[]=value1&name[]=value2` becomes `['name[]' => ['value1', 'value2']] in Shopify.
      * See: https://github.com/rack/rack/blob/f9ad97fd69a6b3616d0a99e6bedcfb9de2f81f6c/lib/rack/query_parser.rb#L36
      *
-     * @param string $queryString The query string.
-     * @param string|null $delimiter  The delimiter.
+     * @param string $qs The query string.
+     * @param string|null $d  The delimiter.
      *
      * @return mixed
      */
-    public static function parseQueryString(string $queryString, string $delimiter = null): array
+    public static function parseQueryString(string $qs, string $d = null): array
     {
-        $commonSeparator = [';' => '/[;]\s*/', ';,' => '/[;,]\s*/', '&' => '/[&]\s*/'];
-        $defaultSeparator = '/[&;]\s*/';
+        $COMMON_SEP = [';' => '/[;]\s*/', ';,' => '/[;,]\s*/', '&' => '/[&]\s*/'];
+        $DEFAULT_SEP = '/[&;]\s*/';
 
         $params = [];
         $split = preg_split(
-            $delimiter ? $commonSeparator[$delimiter] || '/['.$delimiter.']\s*/' : $defaultSeparator,
-            $queryString ?? ''
+            $d ? $COMMON_SEP[$d] || '/['.$d.']\s*/' : $DEFAULT_SEP,
+            $qs ?? ''
         );
 
-        foreach ($split as $part) {
-            if (! $part) {
+        foreach ($split as $p) {
+            if (! $p) {
                 continue;
             }
 
-            [$key, $value] = strpos($part, '=') !== false ? explode('=', $part, 2) : [$part, null];
+            [$k, $v] = strpos($p, '=') !== false ? explode('=', $p, 2) : [$p, null];
 
-            $key = urldecode($key);
-            $value = $value !== null ? urldecode($value) : $value;
+            $k = urldecode($k);
+            $v = $v !== null ? urldecode($v) : $v;
 
-            if (isset($params[$key])) {
-                $cur = $params[$key];
+            if (isset($params[$k])) {
+                $cur = $params[$k];
 
                 if (is_array($cur)) {
-                    $params[$key][] = $value;
+                    $params[$k][] = $v;
                 } else {
-                    $params[$key] = [$cur, $value];
+                    $params[$k] = [$cur, $v];
                 }
             } else {
-                $params[$key] = $value;
+                $params[$k] = $v;
             }
         }
 
